e8c270adf59116d1613083de59ea68838b0331a9
mickytroxxy <mickylyricalkartel@gmail.com>
2025-07-09 22:27:24 +0200

    with latest code

--- android/src/main/java/com/visioncameramrzscanner/VisionCameraMrzScannerPackage.kt
+++ android/src/main/java/com/visioncameramrzscanner/VisionCameraMrzScannerPackage.kt
@@ -4,12 +4,19 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin
+import com.mrousavy.camera.frameprocessors.FrameProcessorPluginRegistry
 
 
 class VisionCameraMrzScannerPackage : ReactPackage {
+  companion object {
+    init {
+      FrameProcessorPluginRegistry.addFrameProcessorPlugin("scanMRZ") { proxy, options ->
+        VisionCameraMrzScannerPlugin(proxy, options)
+      }
+    }
+  }
+
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    FrameProcessorPlugin.register(VisionCameraMrzScannerPlugin())
     return emptyList()
   }
 
--- android/src/main/java/com/visioncameramrzscanner/VisionCameraMrzScannerPlugin.kt
+++ android/src/main/java/com/visioncameramrzscanner/VisionCameraMrzScannerPlugin.kt
@@ -5,7 +5,7 @@ import android.graphics.Point
 import android.graphics.Rect
 import android.media.Image
 import android.util.Log
-import androidx.camera.core.ImageProxy
+
 import com.facebook.react.bridge.WritableNativeArray
 import com.facebook.react.bridge.WritableNativeMap
 import com.google.android.gms.tasks.Task
@@ -14,9 +14,21 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin
+import com.mrousavy.camera.frameprocessors.Frame
+import com.mrousavy.camera.frameprocessors.FrameProcessorPlugin
+import com.mrousavy.camera.frameprocessors.VisionCameraProxy
 
-class VisionCameraMrzScannerPlugin: FrameProcessorPlugin("scanMRZ"){
+class VisionCameraMrzScannerPlugin(proxy: VisionCameraProxy, options: Map<String, Any>?): FrameProcessorPlugin() {
+
+  private fun orientationToDegrees(orientation: String): Int {
+    return when (orientation) {
+      "portrait" -> 0
+      "landscape-left" -> 90
+      "portrait-upside-down" -> 180
+      "landscape-right" -> 270
+      else -> 0
+    }
+  }
 
   private fun getBlockArray(blocks: MutableList<Text.TextBlock>): WritableNativeArray {
     val blockArray = WritableNativeArray()
@@ -70,10 +82,12 @@ private fun getElementArray(elements: MutableList<Text.Element>): WritableNative
         Log.d("VisionCameraMrzScanner", "elements: ${elements.size}")
         for (element in elements) {
             val elementMap = WritableNativeMap()
-    
+
             elementMap.putString("text", element.text)
             elementMap.putArray("cornerPoints", element.cornerPoints?.let { getCornerPoints(it) })
             elementMap.putMap("frame", getFrame(element.boundingBox))
+
+            elementArray.pushMap(elementMap)
         }
     } catch (e: Exception) {
         Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
@@ -133,29 +147,27 @@ private fun getFrame(boundingBox: Rect?): WritableNativeMap {
 }
 
 @SuppressLint("NewApi")
-override fun callback(frame: ImageProxy, params: Array<Any>): Any? {
+override fun callback(frame: Frame, arguments: Map<String, Any>?): Any? {
 
     val result = WritableNativeMap()    
     
     val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    @SuppressLint("UnsafeOptInUsageError")
-    val mediaImage: Image? = frame.getImage()
 
-    if (mediaImage != null) {
-        return try {
-            val image = InputImage.fromMediaImage(mediaImage, frame.imageInfo.rotationDegrees)
-            val task: Task<Text> = recognizer.process(image)
-                val text: Text = Tasks.await<Text>(task)
-                result.putString("text", text.text)
-                result.putArray("blocks", getBlockArray(text.textBlocks))
-                val data = WritableNativeMap()
-                data.putMap("result", result)
-                return data
-        } catch (e: Exception) {
-            Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
-            e.printStackTrace()
-        }
+    return try {
+        // Get the underlying Android Image from the Frame
+        val mediaImage = frame.image
+        val image = InputImage.fromMediaImage(mediaImage, 0)
+        val task: Task<Text> = recognizer.process(image)
+        val text: Text = Tasks.await<Text>(task)
+        result.putString("text", text.text)
+        result.putArray("blocks", getBlockArray(text.textBlocks))
+        val data = WritableNativeMap()
+        data.putMap("result", result)
+        return data
+    } catch (e: Exception) {
+        Log.e("VisionCameraMrzScanner", "Error: ${e.message}")
+        e.printStackTrace()
+        return null
     }
-    return null 
 }
 }
